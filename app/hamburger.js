40697bcb05074483d2005ce35d54d2228968fb10
John Doe <johndoe@example.com>
2023-10-18 00:32:46 +0200

    added mobile nav bar + updated content

---
+++ app/hamburger.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const mobileNav = document.getElementById("mobile-nav");
+    const menuItems = mobileNav.querySelectorAll("li"); // Get all the list items in the mobile navigation
+
+    // Initially hide the mobile navigation
+    mobileNav.style.display = "none";
+    let isActive = false;
+
+    hamburger.addEventListener("click", function () {
+        if (mobileNav.style.display === "block") {
+            mobileNav.style.display = "none";
+            isActive = false;
+        } else {
+            mobileNav.style.display = "block";
+            isActive = true;
+        }
+
+        if (isActive) {
+            hamburger.classList.add("active");
+            
+            // Animate the menu items
+            menuItems.forEach((item, index) => {
+                item.style.animation = `fadeIn 0.3s ease-in forwards ${index * 0.3}s`;
+            }
+        )} else {
+            hamburger.classList.remove("active");
+            // Reset the animation properties when closing the menu
+            menuItems.forEach((item) => {
+                item.style.animation = "";
+            });
+        }
+    });
+});
