7a48efa434ea2d56be3236cf3a310b5123832e70
Oscar <johndoe@example.com>
2023-10-19 23:52:17 +0200

    Last updates

--- app/darkmode.js
+++
@@ -1,9 +0,0 @@
-document
-  .getElementById("toggle-switch")
-  .addEventListener("change", function (event) {
-    if (event.target.checked) {
-      document.body.style.backgroundColor = "unset";
-    } else {
-      document.body.style.backgroundColor = "black";
-    }
-  });
